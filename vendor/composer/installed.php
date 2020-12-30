9147441725acaced3cc69728bbee84e7699dfed2
Rogério José Gomes de Faria <royfaria@gmail.com>
2020-12-30 18:10:04 -0300

    Inicio do projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'rogeriodefaria/rdfoils',
+  ),
+  'versions' => 
+  array (
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'd98ffecfa4832608fab551548d2e032283279e46',
+    ),
+    'rogeriodefaria/rdfoils' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+  ),
+);
