664de1ae8e3f435e0252a2bc41a22bef41de8888
Rogério José Gomes de Faria <royfaria@gmail.com>
2020-12-31 16:49:23 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
-
+	session_start();
 	require_once("vendor/autoload.php");
 
 	  // use \RDFOils\DB\Sql;
 	use \Slim\Slim;
 	use \RDFOils\Page;
 	use \RDFOils\PageAdmin;
+	use \RDFOils\Model\User;
 
 	$app = new Slim();
 
@@ -20,11 +21,39 @@
 
 	$app->get('/admin', function()
 		{
+			User::verifyLogin();
+
 			$page = new PageAdmin();
 
 			$page->setTpl("index");
 		});
 
+	$app->get('/admin/login', function()
+		{
+			$page = new PageAdmin([
+				"header"=>false,
+				"footer"=>false
+			]);
+
+			$page->setTpl("login");
+		});
+
+	$app->post('/admin/login', function()
+		{
+			User::login($_POST["login"], $_POST["password"]);
+
+			header("Location: /admin");
+			exit;
+		});
+
 	$app->run();
+
+	$app->get('/admin/logout', function()
+		{
+			User::logout();
+
+			header("Location: /admin/login");
+			exit;
+		})
 
 ?>
